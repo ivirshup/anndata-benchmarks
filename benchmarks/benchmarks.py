38ca43f1be3ef629ccb123948f40875ef4fe7a5c
Isaac Virshup <ivirshup@gmail.com>
2019-06-03 13:36:32 +1000

    Preliminary IO benchmarks and reorg
    * Added some simple benchmarks for io. Getting some strange results for memory usage, but I think these datasets may be small enough I'm recording overhead.
    * Created util file for storing common utilities
    * Cleanup formatting

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,106 +1,14 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
-from string import ascii_lowercase
-from time import sleep
-
 from memory_profiler import memory_usage
 import numpy as np
-import pandas as pd
-from scipy import sparse
 
-# from .helpers import gen_adata
-
-from anndata import AnnData
-
-###############
-# Helpers
-###############
-
-
-def get_anndata_memsize(adata):
-    # def make_copy(adata):
-    #     sleep(0.005)
-    #     c = adata.copy()
-    #     sleep(0.005)
-    # recording = memory_usage((make_copy, (adata,)), interval=0.001)
-    recording = memory_usage(
-        (sedate(adata.copy, naplength=0.005), (adata,)), interval=0.001)
-    diff = recording[-1] - recording[0]
-    return diff
-
-
-def sedate(func, naplength=0.05):
-    """Make a function sleepy, so we can sample the start and end state.
-    """
-    def wrapped_function(*args, **kwargs):
-        sleep(naplength)
-        val = func(*args, **kwargs)
-        sleep(naplength)
-        return val
-    return wrapped_function
-
-# TODO: Factor out the time it takes to generate these
-
-
-def gen_indexer(adata, dim, index_kind, ratio):
-    dimnames = ("obs", "var")
-    axis = dimnames.index(dim)
-    subset = [slice(None), slice(None)]
-    axis_size = adata.shape[axis]
-    if index_kind == "slice":
-        subset[axis] = slice(0, int(np.round(axis_size * ratio)))
-    elif index_kind == "intarray":
-        subset[axis] = np.random.choice(
-            np.arange(axis_size), int(np.round(axis_size * ratio)), replace=False)
-        subset[axis].sort()
-    elif index_kind == "boolarray":
-        pos = np.random.choice(
-            np.arange(axis_size), int(np.round(axis_size * ratio)), replace=False
-        )
-        a = np.zeros(axis_size, dtype=bool)
-        a[pos] = True
-        subset[axis] = a
-    else:
-        raise ValueError()
-    return tuple(subset)
-
-
-def take_view(adata, *, dim, index_kind, ratio=.5, nviews=100):
-    subset = gen_indexer(adata, dim, index_kind, ratio)
-    views = []
-    for i in range(nviews):
-        views.append(adata[subset])
-
-
-def take_repeated_view(adata, *, dim, index_kind, ratio=.9, nviews=10):
-    v = adata
-    views = []
-    for i in range(nviews):
-        subset = gen_indexer(v, dim, index_kind, ratio)
-        v = v[subset]
-        views.append(v)
-
-
-def gen_adata(n_obs, n_var, attr_set):
-    if "X-csr" in attr_set:
-        X = sparse.random(n_obs, n_var, density=0.1, format="csr")
-    elif "X-dense" in attr_set:
-        X = sparse.random(n_obs, n_var, density=0.1, format="csr")
-        X = X.toarray()
-    else:
-        # TODO: Theres probably a better way to do this
-        X = sparse.random(n_obs, n_var, density=0, format="csr")
-    adata = AnnData(X)
-    if "obs,var" in attr_set:
-        adata.obs = pd.DataFrame(
-            {k: np.random.randint(0, 100, n_obs) for k in ascii_lowercase},
-            index=["cell{}".format(i) for i in range(n_obs)]
-        )
-        adata.var = pd.DataFrame(
-            {k: np.random.randint(0, 100, n_var) for k in ascii_lowercase},
-            index=["gene{}".format(i) for i in range(n_var)]
-        )
-    return adata
+from .utils import (
+    get_anndata_memsize,
+    sedate,
+    gen_indexer, gen_adata,
+    take_view, take_repeated_view
+)
 
 
 class SubsetMemorySuite:
@@ -152,8 +60,8 @@ class SubsetMemorySuite:
 # This is split off from the previous so we don't time index generation, I think this isn't as big an issue for memory usage
 class SubsetTimingSuite:
     params = (
-        [100], #[100, 1000],
-        [100], #[100, 1000],
+        [100],  # [100, 1000],
+        [100],  # [100, 1000],
         ["X-csr", "X-dense", "obs,var"],
         ["obs", "var"],
         ["intarray", "boolarray", "slice"]
---
+++ benchmarks/readwrite.py
@@ -0,0 +1,95 @@
+"""
+This module will benchmark io of AnnData objects
+
+Things to test:
+
+* Read time, write time
+* Peak memory during io
+* File sizes
+
+Parameterized by:
+
+* What method is being used
+* What data is being included
+* Size of data being used
+
+Also interesting:
+
+* io for views
+* io for backed objects
+"""
+import tempfile
+from pathlib import Path
+
+from memory_profiler import memory_usage
+import numpy as np
+
+from .utils import (
+    get_anndata_memsize,
+    sedate
+)
+
+import anndata
+
+PBMC_REDUCED_PATH = "/Users/isaac/github/scanpy/scanpy/datasets/10x_pbmc68k_reduced.h5ad"
+PBMC_3K_PATH = "/Users/isaac/data/pbmc3k_raw.h5ad"
+
+
+class H5ADReadSuite:
+    params = (
+        [PBMC_REDUCED_PATH, PBMC_3K_PATH]
+    )
+    param_names = ["input_path"]
+
+    def setup(self, input_path):
+        self.filepath = input_path
+
+    def time_read_full(self, input_path):
+        anndata.read_h5ad(self.filepath)
+
+    def peakmem_read_full(self, input_path):
+        anndata.read_h5ad(self.filepath)
+
+    def mem_readfull_object(self, input_path):
+        return anndata.read_h5ad(self.filepath)
+
+    def track_read_full_memratio(self, input_path):
+        mem_recording = memory_usage(
+            (
+                sedate(anndata.read_h5ad, .005),
+                (self.filepath,)
+            ),
+            interval=.001
+        )
+        adata = anndata.read_h5ad(self.filepath)
+        base_size = get_anndata_memsize(adata)
+        print(np.max(mem_recording) - np.min(mem_recording))
+        print(base_size)
+        return (np.max(mem_recording) - np.min(mem_recording)) / base_size
+
+
+class H5ADWriteSuite:
+    params = (
+        [PBMC_REDUCED_PATH, PBMC_3K_PATH]
+    )
+    param_names = ["input_path"]
+
+    def setup(self, input_path):
+        self.adata = anndata.read_h5ad(input_path)
+        self.tmpdir = tempfile.TemporaryDirectory()
+        self.writepth = Path(self.tmpdir.name) / "out.h5ad"
+
+    def teardown(self, input_path):
+        self.tmpdir.cleanup()
+
+    def time_write_full(self, input_path):
+        self.adata.write_h5ad(self.writepth, compression=None)
+
+    def peakmem_write_full(self, input_path):
+        self.adata.write_h5ad(self.writepth)
+
+    def time_write_compressed(self, input_path):
+        self.adata.write_h5ad(self.writepth, compression="gzip")
+
+    def peakmem_write_compressed(self, input_path):
+        self.adata.write_h5ad(self.writepth, compression="gzip")
